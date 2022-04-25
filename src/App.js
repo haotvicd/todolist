6ebae7d55885a326a837a0900df8bb30cb2023be
Tran Vu Hao <haotv@icd-vn.com>
2022-04-25 08:47:35 +0700

    update call api

--- src/App.js
+++ src/App.js
@@ -4,21 +4,39 @@ import Header from './components/Header'
 import AddTodo from './components/AddTodo'
 
 function App() {
-  const [todos, setTodos] = useState(() => {
-    return localStorage.getItem('todos') === null ? [] : JSON.parse(localStorage.getItem('todos'))
-  })
+  const [todos, setTodos] = useState([]);
 
   const handleAdd = (todo) => {
-    setTodos([...todos, todo])
+    fetch('http://localhost:5000/todo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todo)
+    })
+      .then(res => res.json())
+      .then(data => setTodos([data, ...todos]))
+      .catch(err => console.log({ message: err.message }));
   }
 
   const handleDelete = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id))
+    fetch(`http://localhost:5000/todo/${id}`, {
+      method: 'DELETE'
+    })
+      .then(() => setTodos(todos.filter(todo => todo._id !== id)))
+      .catch(err => console.log({ message: err.message }));
+  }
+
+  const handleClick = (e) => {
+    console.log(e);
   }
 
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  })
+    fetch('http://localhost:5000/todo')
+      .then(res => res.json())
+      .then(data => setTodos(data))
+      .catch(err => console.log({ message: err.message }));
+  }, [])
 
   return (
     <div className="app">
@@ -27,8 +45,11 @@ function App() {
       <ul className='todolist'>
         {todos.map((todo, index) => (
           <li key={index}>
-            <span>{todo.title}</span>
-            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+            <label className='flex'>
+              <input type='checkbox' checked={todo.completed} onChange={(e) => handleClick(e.target.checked)} />
+              <span>{todo.title}</span>
+            </label>
+            <button onClick={() => handleDelete(todo._id)}>Delete</button>
           </li>
         ))}
       </ul>
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -10,6 +10,7 @@ function AddTodo({ addTodo }) {
     setTitle('')
     e.preventDefault()
   }
+
   return (
     <form onSubmit={handleSubmit} >
       <input type='text' value={title} onChange={e => setTitle(e.target.value)} placeholder='type your plan here...' />
